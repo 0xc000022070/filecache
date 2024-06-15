9502767c69c40f4534a6f601bbeca2cdf3d5228f
Luis Quiñones <lpaandres2020@gmail.com>
2024-06-15 17:13:45 -0500

    test: added dummy test to only check that data is cached and ttl works

---
+++ filecache_test.go
@@ -0,0 +1,64 @@
+package filecache_test
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/0xc000022070/filecache"
+)
+
+func TestBasic(t *testing.T) {
+	cache := filecache.New("basic-test",
+		filecache.WithMaxItems(10),
+		filecache.WithTTL(time.Second*3),
+		filecache.WithCheckInterval(time.Second),
+	)
+
+	type BasicTestData struct {
+		Why string
+	}
+
+	data := BasicTestData{
+		Why: "I'm a life",
+	}
+
+	const key = "internal/basic-test.gob"
+
+	if err := filecache.SetEncoded(cache, key, data); err != nil {
+		t.Fatal(err)
+	}
+
+	ticker := time.NewTicker(time.Second)
+	ticks := 0
+
+	t.Log("The cache data should be removed after 3 seconds")
+
+	for range ticker.C {
+		if ticks >= 5 {
+			t.Log("The cache data wasn't removed")
+			t.Fail()
+
+			break
+		}
+
+		ticks++
+		t.Logf("tick n° %d", ticks)
+
+		data, err := filecache.GetDecoded[BasicTestData](cache, key)
+		if err != nil {
+			if errors.Is(err, filecache.ErrNotFound) {
+				if ticks < 3 {
+					t.Log("the cache data was removed but that was not expected")
+					t.Fail()
+				} else {
+					t.Log("as expected, the cache data was removed")
+				}
+
+				break
+			}
+		}
+
+		t.Log("stored data is", data)
+	}
+}
