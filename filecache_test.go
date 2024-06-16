9e104782f099c20ff3b9ccbdea3f9824c17b1f2c
Luis Quiñones <lpaandres2020@gmail.com>
2024-06-16 12:08:14 -0500

    test: added tests cases to check that destroy and shutdown behaviors are working as expected

--- filecache_test.go
+++ filecache_test.go
@@ -64,3 +64,62 @@ func TestBasic(t *testing.T) {
 		t.Log("stored data is", data)
 	}
 }
+
+func TestDestroy(t *testing.T) {
+	t.Parallel()
+
+	cache := filecache.New("destroy-test")
+	defer cache.Destroy()
+
+	if err := cache.Set("a-misty-value", []byte("I'm a misty value")); err != nil {
+		t.Errorf("failed to set a value in file-based cache: %v", err)
+		t.FailNow()
+	}
+
+	if err := cache.Destroy(); err != nil {
+		t.Errorf("failed to destroy file-based cache: %v", err)
+		t.FailNow()
+	}
+
+	cache = filecache.New("destroy-test")
+
+	data, err := cache.Get("a-misty-value")
+	if err != nil {
+		if errors.Is(err, filecache.ErrNotFound) {
+			t.Log("as expected, the value was removed from the cache")
+			return
+		}
+
+		t.Errorf("failed to retrieve the value from the cache: %v", err)
+		t.FailNow()
+	}
+
+	t.Errorf("failed to remove the value from the cache: %v", err)
+	t.Logf("a-misty-value: %s", data)
+	t.Fail()
+}
+
+func TestShutdown(t *testing.T) {
+	t.Parallel()
+
+	cache := filecache.New("shutdown-test")
+
+	if err := cache.Set("extraordinary", []byte("the extraordinary value")); err != nil {
+		t.Errorf("failed to set a value in file-based cache: %v", err)
+		t.FailNow()
+	}
+
+	cache.Shutdown()
+
+	cache = filecache.New("shutdown-test")
+	defer cache.Destroy()
+
+	data, err := cache.Get("extraordinary")
+	if err != nil {
+		t.Errorf("failed to get the value from the cache when it was suposed to be in file-system: %v", err)
+		t.FailNow()
+	}
+
+	t.Log("as expected, the value was removed from the cache")
+	t.Logf("extraordinary: %s", data)
+}
